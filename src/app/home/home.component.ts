407833169db602f36ad69882adb6095af84cb822
DINESH S <dinisellappan@gmail.com>
2021-01-15 12:47:25 +0530

    host withsafe

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatDialog } from '@angular/material/dialog';
+import { LogoutComponent } from '../logout/logout.component';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
+
 
   ngOnInit(): void {
+  }
+
+  opendialog() {
+    this.dialog.open(LogoutComponent, {width: '500px', height: '450px'});
   }
 
 }
